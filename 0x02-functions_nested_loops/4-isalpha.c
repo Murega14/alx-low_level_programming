691efbab8f2ea2099674a232f30c1731be8e40da
murega14 <tedmurega@gmail.com>
2022-09-14 17:26:56 -0700

    letter check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * function is int _isalpha(Int c)
+ * return 1 if c is a letter,
+ * uppercase or lowercase
+ * return 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >=65 && c <= 90) || (c >=97 && c <= 122))
+		return(1);
+	else
+		return(0);
+}
