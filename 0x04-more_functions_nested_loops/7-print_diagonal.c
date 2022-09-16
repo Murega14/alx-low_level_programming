b82f33ddc04f18bfce35739f67eac5ff88011f56
murega14 <tedmurega@gmail.com>
2022-09-15 17:59:00 -0700

    print a diagonal line on terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,12 +7,19 @@
 
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i = 0, j;
 
 	if (n > 0)
 	{
 		for (; i < n; i++)
-			_putchar('\');
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
+
 }
