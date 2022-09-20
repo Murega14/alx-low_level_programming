89b468009031ed05b4507de07ac6c16b4a5e8140
murega14 <tedmurega@gmail.com>
2022-09-19 19:27:41 -0700

    printing a single character from a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - a function that prints every character of a string
+ * @str: an input string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
