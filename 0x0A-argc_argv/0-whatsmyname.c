5fa3bd90e11a6797a8b72d5ac509f968438ed9b5
murega14 <tedmurega@gmail.com>
2022-10-18 19:14:02 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its own name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b, result;
+
+	if (argc != 3)
+	{
+		printf("Try again\n");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = a * b;
+		printf("%d\n", result);
+		return (0);
+	}
+}
